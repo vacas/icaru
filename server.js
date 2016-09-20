64d58adc1d8f8235317343fdbb4f09f629d09401
Miguel Vacas <mfjvacas@gmail.com>
2016-09-19 23:26:25 -0400

    HOTFIX: console.log fix

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var port = process.env.PORT || 3000;
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(port);
+app.listen(port, function() {
+  console.log("Your port is " + port);
+});
