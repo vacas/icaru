495b1f0196f08711c5789bf88048152116ea241a
Miguel Vacas <mfjvacas@gmail.com>
2016-09-19 23:22:19 -0400

    HOTFIX: Typo on '_dirname'

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use('/', express.static(_dirname + '/phaser-icaru'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(port);
